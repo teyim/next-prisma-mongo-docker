defe4469b9cbcd6a17fdf85831abb44d7b8fb43b
teyim <teyimasobo@gmail.com>
2024-07-21 14:48:05 +0100

    feat: create crud endpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div> hello world, how are you ?</div>
+      <div> hello world, how are you today, hope ur doing fine?</div>
     </main>
   );
 }
